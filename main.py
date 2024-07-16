f569636b0e9e4dfa0ec54ac9b745a99b230501dd
Filip Wądołowski <84192488+fkwadolowski@users.noreply.github.com>
2024-07-17 01:46:01 +0200

    editing and home page working

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
-
+from sqlalchemy import select
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -54,22 +63,44 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
-second_movie = Movie(
-    title="Avatar The Way of Water",
-    year=2022,
-    description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-    rating=7.3,
-    ranking=9,
-    review="I liked the water.",
-    img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-)
-with app.app_context():
-    db.session.add(second_movie)
-    db.session.commit()
+# second_movie = Movie(
+#     title="Avatar The Way of Water",
+#     year=2022,
+#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     rating=7.3,
+#     ranking=9,
+#     review="I liked the water.",
+#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+# )
+# with app.app_context():
+#     db.session.add(second_movie)
+#     db.session.commit()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with app.app_context():
+        result = db.session.execute(db.select(Movie))
+        all_movies = result.scalars().all()
+    return render_template("index.html", data=all_movies)
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    form=RateMovieForm()
+    movie_id=request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        with app.app_context():
+            movie_to_update = db.session.execute(db.select(Movie).filter(Movie.id == movie_id)).scalar_one_or_none()
+            movie_to_update.rating=float(form["rating"].data)
+            movie_to_update.review=str(form["review"].data)
+            db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html",movie=movie, form=form)
+
 
 
 if __name__ == '__main__':
